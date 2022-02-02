f67c54e07037682d72c6b11e5af0ac9254a06aeb
Usuario <Usuario@LAPTOP-R7I2TBK9>
2022-02-02 17:03:26 -0600

    Ejercicio3

---
+++ CursoSelenium/src/test/java/com/java/Ejercicio3.java
@@ -0,0 +1,29 @@
+package com.java;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int edad;
+		String codigopostal; //nivel mundial
+		double altura;
+		char genero; //valores H:hombre o M:mujer
+		String nombre;
+		int numerodehijos;
+		double iva;
+		int tallacamisa; //valor numerico de talle
+		double peso;
+		double precio;
+		char alumnoRepetidor; //valores si/no
+		String mensaje;
+		char letra;
+		boolean mayorEdad;
+		int minutos;
+		int dias;
+		String matriculaCoche;
+		int contador;
+		boolean mayorDeEdad;
+		char tallaCamiseta; //valores de talle en letras
+}
+}
