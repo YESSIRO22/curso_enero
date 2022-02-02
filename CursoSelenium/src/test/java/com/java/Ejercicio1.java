c0f990b265a15d026525a2385d1da4e080d24bd4
Usuario <Usuario@LAPTOP-R7I2TBK9>
2022-02-01 19:19:39 -0600

    Ejercicio1

---
+++ CursoSelenium/src/test/java/com/java/Ejercicio1.java
@@ -0,0 +1,22 @@
+package com.java;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// declaracion de variables
+		double a=10.14;
+		double b=9.09;
+		double c=3.45;
+		
+		double resta=a+b+c;
+		double multiplicacion=(a*b)*c;
+		double division=(a/b)/c;
+		
+		System.out.println("La resta de los numeros es "+ resta);
+		System.out.println("La multiplicacion de los numeros es "+ multiplicacion);
+		System.out.println("La division de los numeros es "+ division);
+		
+
+	}
+
+}
