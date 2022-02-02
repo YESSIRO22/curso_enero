85af64dbb96daa95b32d4f04e544e5cad86f3cc3
Usuario <Usuario@LAPTOP-R7I2TBK9>
2022-02-02 17:23:01 -0600

    Ejercicio4

---
+++ CursoSelenium/src/test/java/com/java/Ejercicio4.java
@@ -0,0 +1,20 @@
+package com.java;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nro1=20;
+		int nro2=20;
+		
+		if(nro1==nro2)
+		{
+			System.out.println("Los numeros son iguales");
+		} else if (nro1>nro2) {
+			System.out.println("El numero "+nro1+" es mayor que el numero "+nro2);
+		}else {
+			System.out.println("El numero "+nro2+" es mayor que el numero "+nro1);
+		}
+		}
+
+	}
